dde53fca2c19ca58b974d02fac9c891fc987ea24
Vasco Hinostroza <82005643+theocsav@users.noreply.github.com>
2025-12-02 15:06:03 -0500

    Export traces and metrics to Jaeger via OTLP

--- tracing.js
+++ tracing.js
@@ -1,12 +1,15 @@
+const { OTLPTraceExporter } =
+  require('@opentelemetry/exporter-trace-otlp-http');
+const { OTLPMetricExporter } =
+  require('@opentelemetry/exporter-metrics-otlp-http');
+
 const { resourceFromAttributes } = require('@opentelemetry/resources');
 const { ATTR_SERVICE_NAME } = require('@opentelemetry/semantic-conventions');
 const { NodeSDK } = require('@opentelemetry/sdk-node');
 const {
   getNodeAutoInstrumentations,
 } = require('@opentelemetry/auto-instrumentations-node');
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
 const {
-  ConsoleMetricExporter,
   PeriodicExportingMetricReader,
 } = require('@opentelemetry/sdk-metrics');
 
@@ -18,13 +21,21 @@ const { MongoDBInstrumentation } =
 const { HttpInstrumentation } =
   require("@opentelemetry/instrumentation-http");
 
+const traceExporter = new OTLPTraceExporter({
+  url: 'http://localhost:4318/v1/traces',
+});
+
+const metricExporter = new OTLPMetricExporter({
+  url: 'http://localhost:4318/v1/metrics',
+});
+
 const sdk = new NodeSDK({
   resource: resourceFromAttributes({
     [ATTR_SERVICE_NAME]: "todo-service",
   }),
-  traceExporter: new ConsoleSpanExporter(),
+  traceExporter,
   metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter(),
+    exporter: metricExporter,
   }),
   instrumentations: [
     getNodeAutoInstrumentations(),
